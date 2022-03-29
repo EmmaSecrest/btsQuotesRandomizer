a02244d780e18505b5689d4c356d4213e738e369
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2022-03-29 16:24:28 -0400

    started quotes database

---
+++ src/quotesDatabase/database.js
@@ -0,0 +1,342 @@
+const quotes =[
+    {
+        quote:"To be honest, I'm cuter than him,especially now with my bangs",
+        speaker:'Park Jimin'
+    },
+    {
+       quote: "It may feel like the world is collapsing when you don't get the results you want. But that's not true. That's not how our lives work. What's important is you get back up" ,
+       speaker: 'Min Yoongi on VLive'
+    },
+    {
+        quote:"I wish there was a better word than ‘love.’ I really, truly love you. Please know that",
+        speaker:'Kim Namjoon'
+    },
+    {
+        quote:"There’s 100% of success, I’ll give 50, half to ARMYs. The other half, we put like 5% each, so that’s 35. And I give 15% to the co. So if it’s a trophy, my % is like 5, only an edge of the trophy. Success is not all ours I always try to realize that",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"I live to Understand the world, but the world has never understood me",
+        speaker:"Kim Namjoon"
+    },
+    {
+       quote:"Maybe I made a mistake yesterday, but yesterday's me is still me. Today I am who I am with all of my mistakes & faults. Tomorrow, I might be a tiny bit wiser and that would still be me too.",
+       speaker: "Kim Namjoon from UN unicef speech in 2019" 
+    },
+    {
+        quote:"If any of you feels lost in the face of doubt or uncertainty, or the pressure of starting anew, don't rush. Take a deep breath. You may find that any moment can be turned into an opportunity. Allow yourself to take it easy. Take it one step at a time.",
+        speaker:"Kim Seokjin from Dear class of 2020 YouTube commencement speech"
+    },
+    {
+        quote:"I don't have think",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"Remember there is a person here in Korea, in the city of Seoul, who understands you.",
+        speaker:"Park Jimin from Dear Class of 2020"
+    },
+    {
+        quote:"My name is R-A-P monster not a D-A-N-C-E monster",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"If we could reduce your pain from 100 to 99, 98 or 97, that makes our existence worthy.",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"Art isn’t hard. It’s right next to us",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"I know her face but I don't know her name",
+        speaker:"Jeon JungKook"
+    },
+    {
+        quote:"If you think you'll crash, accelerate more you idiot",
+        speaker:'Min Yoongi from Intro: Nevermind'
+    },
+    {
+        quote:"Life is tough, and things don't always work out well, but we should be brave and go on with our lives. ",
+        speaker:"Min Yoongi on VLive"
+    },
+    {
+        quote:"Not everyone needs to have a dream. You should live your life your own way.",
+        speaker:"Min Yoongi on VLive"
+    },
+    {
+        quote:"You were born to be real, not perfect",
+        speaker:"Min Yoongi from a concert Ment"
+    },
+    {
+        quote:"You know BTS?",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"Don't be trapped in someone else's dream.",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"Effort makes you. You will regret someday if you don’t do your best now. Don’t think it’s too late but keep working on it. It may take time, but there’s nothing that gets worse due to practising. So practise. You may get depressed, but its evidence that you are doing good.",
+        speaker:"Jeon JungKook"
+    },
+    {
+        quote:"If you can't respect, don't even open your mouth",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"You can't just come into someone's life, make them feel special, and then leave.",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"You can't just come into someone's life, make them feel special, and then leave.",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"Living without passion is like being dead.",
+        speaker:"Jeon JungKook"
+    },
+    {
+        quote:"Go on your path, even if you live for a day.",
+        speaker:"Park Jimin"
+    },
+    {
+         quote:"Purple is the last color of the rainbow. Purple means I will trust and love you for a long time.",
+         speaker:"Kim Taehyung"
+    },
+    {
+        quote:"When something is delicious. It's zero calories.",
+        speaker:"Kim Seokjin"
+    },
+    {
+       quote:"If we all gather our embarrassment together, it’ll become confidence.",
+       speaker:"Kim Seokjin" 
+    },
+    {
+        quote:"When things get tough, look at the people who love you! You will get energy from them",
+        speaker:"Jung Hoseok"
+    },
+    {
+        quote:"Without anger or sadness, you won’t be able to feel true happiness.",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"OUR ARMY",
+        speaker:"Jung Hoseok on Weverse 2022"
+    },
+    {
+        quote:"Covid get the hell out",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"My existence itself is A-cut quality",
+        speaker:"Kim Seokjin 2020"
+    },
+    {
+        quote:"Do you want to see my cat?",
+        speaker:"Jeon Jungkook on VLive"
+    },
+    {
+        quote:"It feels like this bundle of worries have been lifted from my mind with today marking the start of even better days to come!",
+        speaker:"Jung Hoseok on Weverse"
+    },
+    {
+        quote:"Is your iron hand ready?",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"You need to have a younger mindset to become younger.",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"He was named after me. He is my best friend",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"Even if it's a group just like us, a lot of them don't get along",
+        speaker:"Jeon Jungkook In Break The Silence : DOCU-SERIES"
+    },
+    {
+        quote:"As expected, It's indeed faster to become BTS than to succeed at ticketing for BTS",
+        speaker:'Kim Seokjin'
+    },
+    {
+        quote:"In our concert we never have no empty seats.",
+        speaker:"Min Yoongi Lyrics in Mic Drop"
+    },
+    {
+        quote:"We work extra hard too!",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"There was a time when ARMY gave us a gift. Using purple army bombs.",
+        speaker:"Park Jimin"
+    },
+    {
+        quote:"Everyone, BTS is strong. You have to be careful!",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"If they talk about it openly — if they talk about depression for example like it’s the common cold, then it becomes more and more accepted. I think artists or celebrities who have a voice should talk about these problems and bring it up to the surface.",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"All I could say is that my face was fancy",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"RM takes care of us when we wok as a team. But privately, the six of us take care of RM.",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"Win, lose, I don't care because at the end of the day I still have this face so who's the real winner here?",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"My handsomeness is staining my life",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"I am worldwide handsome",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"Do you know how hard it is to write on a flip phone, huh?",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"I wish I could come here with my girlfriend... but I don't have one",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"I didn't know fire could be that hot",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"English is not a barrier when you're as cute as me",
+        speaker:"Park Jimin"
+    },
+    {
+        quote:"I was born in Busan first",
+        speaker:"Park Jimin"
+    },
+    {
+        quote:"Don't worry I'm not afraid",
+        speaker:"Park Jimin"
+    },
+    {
+        quote:"Nice paprika",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"Jimine Pabo",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"Even when I'm in the bathroom, I'm thinking of out ARMY",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"Sexy Porn star(saxophonist)",
+        speaker:"Kim Taehyung"
+    },
+    {
+        quote:"In my next life I want to be born as a rock",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"Suga, genius. These 2 words are enough",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"I hate snakeu",
+        speaker:"Jung Hoseok"
+    },
+    {
+        quote:"I'm very loser",
+        speaker:"Jung Hoseok"
+    },
+    {
+        quote:"I'm you're hope, I'm you're angel",
+        speaker:"Jung Hoseok"
+    },
+    {
+        quote:"Dirty water in my faceu",
+        speaker:"Jung Hoseok"
+    },
+    {
+        quote:"I am not your oppa",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"My name is Jungkook..scale is international playboy",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"I know face but I don't know name, I know movie but I don't know movie name",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"Ding Dong",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"Waking up is the second hardest thing in the morning.",
+        speaker:"Kim Namjoon"
+    },
+    {
+        quote:"Tooonnnyyyy!",
+        speaker:"Park Jimin"
+    },
+    {
+        quote:"Jin is weak and old.",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"Hannah Montana says nobody’s perfect, but here I am.",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"I’ve changed my name to J-Horse now.",
+        speaker:"Jung Hoseok"
+    },
+    {
+        quote:"Justin Seagull",
+        speaker:"Jeon Jungkook"
+    },
+    {
+        quote:"Cheaters never win, but I just graduated.",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"R-Revolution E-Evolution D-Drop in the ocean/drobbleution",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"P-A-S-T-A Pasta P-I-Z-Z-A pizza",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"I don't have friends….Ahh BTS! my friends!",
+        speaker:"Kim Seokjin"
+    },
+    {
+        quote:"Jhoooooope",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"Infires Man!",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"Jjang Jjang Man Boong Boong",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"Uh huh listen, boy. My first love story",
+        speaker:"Min Yoongi"
+    },
+    {
+        quote:"I like puppy",
+        speaker:"Min Yoongi"
+    }
+]
