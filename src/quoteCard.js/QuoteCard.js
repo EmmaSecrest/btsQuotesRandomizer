8e32867afe560301c7786c9274c8c9ff787ca0ea
Emma Secrest <emma@Emmas-MBP.attlocal.net>
2022-04-11 16:00:44 -0400

    social media share buttons added

--- src/quoteCard.js/QuoteCard.js
+++ src/quoteCard.js/QuoteCard.js
@@ -7,6 +7,53 @@ import { quotes } from '../quotesDatabase/database';
 import { useState } from 'react';
 import { Image } from 'mui-image';
 import { Button } from '@material-ui/core';
+// import Grid from '@mui/material/Grid';
+
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  HatenaShareButton,
+  InstapaperShareButton,
+  LineShareButton,
+  LinkedinShareButton,
+  LivejournalShareButton,
+  MailruShareButton,
+  OKShareButton,
+  PinterestShareButton,
+  PocketShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  TumblrShareButton,
+  TwitterShareButton,
+  ViberShareButton,
+  VKShareButton,
+  WhatsappShareButton,
+  WorkplaceShareButton
+} from "react-share";
+import {
+  EmailIcon,
+  FacebookIcon,
+  FacebookMessengerIcon,
+  HatenaIcon,
+  InstapaperIcon,
+  LineIcon,
+  LinkedinIcon,
+  LivejournalIcon,
+  MailruIcon,
+  OKIcon,
+  PinterestIcon,
+  PocketIcon,
+  RedditIcon,
+  TelegramIcon,
+  TumblrIcon,
+  TwitterIcon,
+  ViberIcon,
+  VKIcon,
+  WeiboIcon,
+  WhatsappIcon,
+  WorkplaceIcon
+} from "react-share";
+import { Grid } from '@mui/material';
 
 
 export default function QuoteCard(){
@@ -26,6 +73,7 @@ export default function QuoteCard(){
       4.instagram? not exactly sure how that would work since this is just text 
       5. Weverse
    */
+    const shareString = quotes[quoteIndex].quote + '  -' + quotes[quoteIndex].speaker;
     
     return(
         
@@ -63,9 +111,78 @@ export default function QuoteCard(){
               justifyContent="center"
              
             >
-              <Button 
-                variant="outlined" 
-                style ={{ color: "#d8c1e0" , fontFamily:"Arial", border:'1px solid #d8c1e0'  }} onClick = {() => getNewQuote()} > New Quote</Button>
+              <Grid
+                container
+                spacing={3}
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid item>
+                  <Button 
+                    variant="outlined" 
+                    style ={{ color: "#d8c1e0" , fontFamily:"Arial", border:'1px solid #d8c1e0'  }} onClick = {() => getNewQuote()} > New Quote</Button> <br/>
+                </Grid>
+                <Grid item container direction = 'horizontal' justifyContent = 'center'>
+                    <Grid item>
+                    <FacebookShareButton 
+                          quote={quotes[quoteIndex].quote + '  -' + quotes[quoteIndex].speaker}
+                          url= 'secret.com' // change later
+                          hashtag='#btsARMY #btsQuotesRandomizer'
+                          > 
+                        <FacebookIcon />
+                        </FacebookShareButton>
+                    </Grid>
+                    <Grid item>
+                      <LinkedinShareButton
+                        title= {shareString}
+                        summary= "Check out more at BTSquotesRandomizer"
+                        source='BTS Quotes Randomizer'
+                        url="google.com" // change later
+                      >
+                        <LinkedinIcon/>
+                      </LinkedinShareButton>
+                    </Grid>
+                    <Grid item>
+                      <EmailShareButton 
+                        subject='BTS Quote of the Day!'
+                        body={quotes[quoteIndex].quote + '  -' + quotes[quoteIndex].speaker}
+                        url='test.com' // change later
+                        target = '_self'
+                        rel="noopener noreferrer"
+                      >
+                        <EmailIcon />
+                      </EmailShareButton>
+                    </Grid>
+                    <Grid item>
+                      <RedditShareButton
+                        title= {quotes[quoteIndex].quote + '  -' + quotes[quoteIndex].speaker}
+                        url ="test.com" // change later
+                      >
+                        <RedditIcon/>
+                      </RedditShareButton>
+                    </Grid>
+                    <Grid item>
+                     <TwitterShareButton
+                      title = {quotes[quoteIndex].quote + '  -' + quotes[quoteIndex].speaker}
+                      hashtags = {['BTSarmy' , 'BTSquotesRandomizer' ,"BTS"]}
+                      url ='test.com' // change later
+                     >
+                       <TwitterIcon />
+                     </TwitterShareButton>
+                    </Grid>
+                    <Grid item> 
+                      <WhatsappShareButton
+                        title = {quotes[quoteIndex].quote + '  -' + quotes[quoteIndex].speaker}
+                        url = 'test.com' // change later
+                      >
+                        <WhatsappIcon />
+                      </WhatsappShareButton>
+                    </Grid>
+                </Grid>
+                
+              </Grid>
+               
               {/* <Button variant="outlined">Secondary action</Button>  */}
             </Stack>
           </Container>
